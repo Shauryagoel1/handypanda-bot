81e508a32f8bca94af8740ea5953ae0514da070c
Shaurya Goel <shaukage@gmail.com>
2025-05-17 23:06:28 +0530

    more base logs

---
+++ config.py
@@ -0,0 +1,8 @@
+# File: config.py
+
+# Google Sheets configuration
+GOOGLE_CREDENTIALS_FILE = "jirago-d8eff9ce83d3.json"
+GOOGLE_SHEET_TITLE = "Jirago Ops"
+CATALOGUE_TAB = "Catalogue"
+ORDERS_TAB = "Orders_Status"
+ORDERS_LOG_TAB = "Orders_Log"  # New tab for raw message logging 
